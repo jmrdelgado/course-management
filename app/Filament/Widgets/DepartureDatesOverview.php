23c6e0471dc5fdace9091f2a754aeac343095286
Jose Manuel Rufo <jmrufo@openges.es>
2024-11-06 17:48:26 +0100

    Se implementan contadores en el Dashboard para indicar el número de cursos programados por cada fecha de salida

---
+++ app/Filament/Widgets/DepartureDatesOverview.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Carbon\Carbon;
+
+use App\Models\Programming;
+
+class DepartureDatesOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $fechaActual = Carbon::now();
+        $records = Programming::whereYear('start_date', $fechaActual->year)->whereMonth('start_date', $fechaActual->month)
+            ->selectRaw('start_date, COUNT(*) AS totalreg')
+            ->groupBy('start_date')->orderBy('start_date')->get();
+
+        $paneles = [];
+
+        foreach ($records as $value) {
+            $paneles [] = Stat::make('SALIDA DEL ' . Carbon::parse($value->start_date)->format('d-m-Y'), $value->totalreg)->description('Total de Cursos Previstos')->color('success');
+        }
+
+        return $paneles;
+    }
+}
