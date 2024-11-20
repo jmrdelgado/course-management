c1792ace2255cd09c6d7574765a8fe15f9aa0f09
Jose Manuel Rufo <jmrufo@openges.es>
2024-11-20 11:35:31 +0100

    Se añaden rutas para aplicar optimización de aplicación en producción

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', function () {
     /* return view('welcome'); */
     return redirect('/adminpanel');
 });
+
+Route::get('/optimize', function() {
+    Artisan::call('optimize');
+});
+
+Route::get('/notptimize', function() {
+    Artisan::call('optimize:clear');
+});
