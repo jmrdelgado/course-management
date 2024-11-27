d4107ec20c30627934c73bc6deca4d3904414b24
Jose Manuel Rufo <jmrufo@openges.es>
2024-11-27 16:48:29 +0100

    Se implementa ruta para ejecutar comandos de despliegue

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,23 @@ Route::get('/', function () {
     return redirect('/adminpanel');
 });
 
+Route::get('/launchnotification', function() {
+    Artisan::call('queue:work --tries=3 --stop-when-empty');
+});
+
 Route::get('/optimize', function() {
     Artisan::call('optimize');
 });
 
 Route::get('/notptimize', function() {
     Artisan::call('optimize:clear');
+});
+
+Route::get('/deploy', function () {
+    Artisan::call('key:generate');
+    Artisan::call('cache:clear');
+    Artisan::call('optimize');
+    Artisan::call('migrate:fresh');
+    Artisan::call('shield:generate --all');
+    Artisan::call('db:seed');
 });
