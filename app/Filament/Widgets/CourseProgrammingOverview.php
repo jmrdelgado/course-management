320107208f68114dcbc3ded207f86f4e0e1a8587
Jose Manuel Rufo <jmrufo@openges.es>
2024-12-31 10:12:21 +0100

    Corrección Título paneles dashboard

--- app/Filament/Widgets/CourseProgrammingOverview.php
+++ app/Filament/Widgets/CourseProgrammingOverview.php
@@ -14,9 +14,9 @@ class CourseProgrammingOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('CURSOS PROGRAMADOSsss', Programming::all()->count())->description('Total cursos comunicados')->descriptionIcon('heroicon-o-check-badge')->color('success'),
-            Stat::make('CURSOS INCIDENTADOSsss', Programming::query()->where('incident', 1)->count())->description('Total cursos incidentados')->descriptionIcon('heroicon-o-exclamation-triangle')->color('warning'),
-            Stat::make('CURSOS ANULADOSsss', Programming::query()->where('canceled', 1)->count())->description('Total cursos anulados')->descriptionIcon('heroicon-s-no-symbol')->color('danger'),
+            Stat::make('CURSOS PROGRAMADOSss', Programming::all()->count())->description('Total cursos comunicados')->descriptionIcon('heroicon-o-check-badge')->color('success'),
+            Stat::make('CURSOS INCIDENTADOSss', Programming::query()->where('incident', 1)->count())->description('Total cursos incidentados')->descriptionIcon('heroicon-o-exclamation-triangle')->color('warning'),
+            Stat::make('CURSOS ANULADOSss', Programming::query()->where('canceled', 1)->count())->description('Total cursos anulados')->descriptionIcon('heroicon-s-no-symbol')->color('danger'),
         ];
     }
 }
