c08b3d9375b17dbfda87bd391b36de575e32bf29
Jose Manuel Rufo <jmrufo@openges.es>
2025-01-07 14:45:22 +0100

    Corrección errata texto en marcadores

--- app/Filament/Widgets/CourseProgrammingOverview.php
+++ app/Filament/Widgets/CourseProgrammingOverview.php
@@ -14,9 +14,9 @@ class CourseProgrammingOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('CURSOS PROGRAMADOSss', Programming::all()->count())->description('Total cursos comunicados')->descriptionIcon('heroicon-o-check-badge')->color('success'),
-            Stat::make('CURSOS INCIDENTADOSss', Programming::query()->where('incident', 1)->count())->description('Total cursos incidentados')->descriptionIcon('heroicon-o-exclamation-triangle')->color('warning'),
-            Stat::make('CURSOS ANULADOSss', Programming::query()->where('canceled', 1)->count())->description('Total cursos anulados')->descriptionIcon('heroicon-s-no-symbol')->color('danger'),
+            Stat::make('CURSOS PROGRAMADOS', Programming::all()->count())->description('Total cursos comunicados')->descriptionIcon('heroicon-o-check-badge')->color('success'),
+            Stat::make('CURSOS INCIDENTADOS', Programming::query()->where('incident', 1)->count())->description('Total cursos incidentados')->descriptionIcon('heroicon-o-exclamation-triangle')->color('warning'),
+            Stat::make('CURSOS ANULADOS', Programming::query()->where('canceled', 1)->count())->description('Total cursos anulados')->descriptionIcon('heroicon-s-no-symbol')->color('danger'),
         ];
     }
 }
